85636ab965c526607affeca4af53fac9230dc842
Zach R <zach822@gmail.com>
2020-09-18 19:31:36 -0700

    remove gromment and and sass and custom styles

--- countdown_keypad/src/App.js
+++ countdown_keypad/src/App.js
@@ -1,7 +1,8 @@
 import React, { useCallback, useState } from 'react';
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 import Button from './button.js';
+import './styles/app.scss';
 
 function App() {
   const [value, setValue] = useState("");
@@ -33,24 +34,45 @@ function App() {
   }
 
   return (
-    <div className="App">
+    <div className="App" style={appStyle} >
       <header className="App-header">
-        {isSuccess ? <h1>Success!!!!!!!!!!!!!!</h1> : <h1>Some Value = {value}</h1>}
-        <Button number={0} onClick={() => appendNumber(0)} disabled={btnDisabled} />
-        <Button number={1} onClick={() => appendNumber(1)} disabled={btnDisabled} />
-        <Button number={2} onClick={() => appendNumber(2)} disabled={btnDisabled} />
-        <Button number={3} onClick={() => appendNumber(3)} disabled={btnDisabled} />
-        <Button number={4} onClick={() => appendNumber(4)} disabled={btnDisabled} />
-        <Button number={5} onClick={() => appendNumber(5)} disabled={btnDisabled} />
-        <Button number={6} onClick={() => appendNumber(6)} disabled={btnDisabled} />
-        <Button number={7} onClick={() => appendNumber(7)} disabled={btnDisabled} />
-        <Button number={8} onClick={() => appendNumber(8)} disabled={btnDisabled} />
-        <Button number={9} onClick={() => appendNumber(9)} disabled={btnDisabled} />
-        <br />
-        <button onClick={clearValue} style={{ backgroundColor: "red"}}>Clear</button>
+        <div background="dark-1" align="center" style={{margin: "auto"}, {width: "100%"}}>
+        {isSuccess ? <h1 style={{margin: "auto"}, {width: "100%"}}>Success!!!!!!!!!!!!!!</h1> 
+        : <h1 style={{margin: "auto"}, {width: "100%"}} alignSelf="center" textAlign="center">Code: {value}</h1>}
+        <div className="answerBox"></div>
+        <div className="answerBox"></div>
+        <div className="answerBox"></div>
+        <div className="answerBox"></div>
+        </div>
+        <div style={btnContainer}>
+
+          <Button number={1} onClick={() => appendNumber(1)} disabled={btnDisabled} />
+          <Button number={2} onClick={() => appendNumber(2)} disabled={btnDisabled} />
+          <Button number={3} onClick={() => appendNumber(3)} disabled={btnDisabled} />
+          <br/>
+          <Button number={4} onClick={() => appendNumber(4)} disabled={btnDisabled} />
+          <Button number={5} onClick={() => appendNumber(5)} disabled={btnDisabled} />
+          <Button number={6} onClick={() => appendNumber(6)} disabled={btnDisabled} />
+          <br/>
+          <Button number={7} onClick={() => appendNumber(7)} disabled={btnDisabled} />
+          <Button number={8} onClick={() => appendNumber(8)} disabled={btnDisabled} />
+          <Button number={9} onClick={() => appendNumber(9)} disabled={btnDisabled} />
+          <br/>
+          <Button number={0} onClick={() => appendNumber(0)} disabled={btnDisabled} />
+          {/* <Button number={"Clear"} onClick={() => clearValue()} disabled={btnDisabled} /> */}
+          <br />
+        </div>
+          <button onClick={clearValue} style={{ backgroundColor: "red"}}>Clear</button>
       </header>
     </div>
   );
 }
 
 export default App;
+
+const appStyle = {
+  // backgroundColor: "gray" 
+}
+const btnContainer = {
+  textAlign: "center"
+}
--- countdown_keypad/src/button.js
+++ countdown_keypad/src/button.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
+import './styles/buttonStyle.scss';
 
-function Button({number, onClick, disabled}) {
-  return (
-    <div>
-        <button value={number} onClick={onClick} disabled={disabled} style={{cursor:"pointer"}}>{number}</button>
-    </div>
-  );
+function Button({ number, onClick, disabled }) {
+    return (
+        <div style={{ display: "inline" }}>
+            <button label={number.toString()} value={number} onClick={onClick}
+                disabled={disabled} className="btnStyle">{number}</button>
+        </div>
+    );
 }
 
 export default Button;
