ccb343850f2f26f8f6769884058186efe0494c5a
zrobinson <zach822@gmail.com>
2020-12-02 22:39:19 -0600

    Updates

---
+++ precache-manifest.9cb31ac8ac3edd873cfa7f2ac135087b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8b4afd3904ecdc14938e9b6e9a300cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15ecd529cfc47d5fd4b7",
+    "url": "/static/css/main.6aff973e.chunk.css"
+  },
+  {
+    "revision": "ef149c87935b316e74a8",
+    "url": "/static/js/2.06f6f178.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.06f6f178.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15ecd529cfc47d5fd4b7",
+    "url": "/static/js/main.f5512859.chunk.js"
+  },
+  {
+    "revision": "d44d4d5a48cedd68dbf6",
+    "url": "/static/js/runtime-main.2229711a.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "3afabe55a9c566c84d6b7f2e9b2b6932",
+    "url": "/static/media/metalBackground.3afabe55.jpg"
+  }
+]);
