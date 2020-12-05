0e0a864a5369aa8299c3d093203ef6f8ef8c90f6
zrobinson <zach822@gmail.com>
2020-12-04 21:10:10 -0600

    adjust success and set a failure state

--- countdown_keypad/src/App.js
+++ countdown_keypad/src/App.js
@@ -11,33 +11,50 @@ function App() {
   const [guess, setGuess] = useState([]);
   const [correctValue, setCorrectValue] = useState([1,2,3,4]);
   const [isSuccess, setIsSuccess] = useState(false);
+  const [isMissionFailure, setIsMissionFailure ] = useState(false);
   const [btnDisabled, setBtnDisabled] = useState(false);
+  const [isToriWin, setIsToriWin] = useState(false);
 
 
   function appendNumber(number){
     // var newValue = parseInt(value.toString() + number.toString());
     var newVal = guess.concat(number);
     setGuess(newVal);
-    guess.length + 1 < correctValue.length ? setBtnDisabled(false) : setBtnDisabled(true);
-    isGuessCorrect(newVal) ? setIsSuccess(true) : setIsSuccess(false);
+    guess.length + 1 < correctValue.length ? setBtnDisabled(false) : inputLengthReached(newVal);
+  }
+
+  function inputLengthReached(newVal){
+    setBtnDisabled(true)
+    isGuessCorrect(newVal) ? setIsSuccess(true) : makeMissionFailure();
+  }
+
+  function makeMissionFailure() {
+    setIsSuccess(false);
+    setIsMissionFailure(true);
   }
 
   function isGuessCorrect(val){
-    return JSON.stringify(val) === JSON.stringify(correctValue) ? true : false;
+    return JSON.stringify(val) === JSON.stringify(correctValue) ? true : JSON.stringify(val) === JSON.stringify([1,9,9,9]) ? toriWin() : false;
   }
 
+  function toriWin(){
+    setIsToriWin(true);
+    return false;
+  }
   function clearValue(){
     setGuess([]);
     setIsSuccess(false);
     setBtnDisabled(false);
+    setIsMissionFailure(false);
+    setIsToriWin(false);
   }
 
   return (
     <div className="App" >
       <header className="App-header">
         <div background="dark-1" align="center" style={{margin: "auto"}, {width: "100%"}}>
-        {isSuccess ? <h1 className="successHeading">Success!!!!</h1>
-        :   
+        {isSuccess ? <h1 className="digitalClock digitalFont">Win</h1>
+        : isToriWin ? <h1 className="digitalClock digitalFont">tori</h1> : isMissionFailure ? <h1 className="digitalClock digitalFont">lose</h1> :
       <Timer      
         initialTime={1000 * 60 * 15}
         direction="backward"
