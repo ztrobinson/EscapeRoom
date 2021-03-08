86cd1631d79a8d14858f473ff6af0dcff3c4fc73
Zachary Robinson <zach822@gmail.com>
2021-03-07 20:46:57 -0600

    resize window, focus on text imput on start, and add blinking icon to text input

--- escape_hacker_terminal_python/hacker_terminal.py
+++ escape_hacker_terminal_python/hacker_terminal.py
@@ -42,7 +42,6 @@ clue = [
 ]
 
 
-
 #Functions
 def say_my_name():
     welcome_message.value = my_name.value
@@ -59,23 +58,36 @@ def rtnCannedResponse():
     rsps.append(canResponse[randrange(len(canResponse))])
 
 def btnClick():
-    print("button was clicked")
     #Check if answer matches the current stage
     #if match, increment stage and move on
     if checkAnswer(stage):
+        rsps.clear()
         rsps.append(clue[0])
         #if fail, send canned response
     else:
         rtnCannedResponse()
     
 def checkAnswer(stageVal):
-    if txt.value == answer[0]:
+    if txt.value[:-1] == answer[0]:
         return True
     else:
         return False
+
+dotValue = True
+def txtLoopV2():
+    global dotValue
+    txt.value = txt.value[:-1]
+    if dotValue:
+        txt.append("▯")
+        dotValue = False
+    else:
+        txt.append("▮")
+        dotValue = True
+
+
     
 #Main
-app = App(title="Escape Room", bg="#000000")
+app = App(title="Escape Room", bg="#000000", height=800, width=1000)
 
 box = Box(app, width="fill", align="top")
 rsps = TextBox(app, text="Send...", height="fill", multiline=True, width="fill")
@@ -87,10 +99,12 @@ rsps.disable()
 
 
 submissionBox = Box(app, width="fill", align="bottom")
-txt = TextBox(submissionBox, text="type here", width="fill", align="left")
+txt = TextBox(submissionBox, text="▮", width="fill", align="left")
 txt.text_color = _fontColor
 txt.text_size = _fontSize
 txt.font = _font
+txt.focus()
+txt.repeat(400, txtLoopV2)
 
 btn = PushButton(submissionBox, text="Send...", align="right", command=btnClick)
 btn.text_color = _fontColor
@@ -99,5 +113,5 @@ btn.font = _font
 btn.height = 0
 
 thing_off(21)
-app.set_full_screen()
+# app.set_full_screen()
 app.display()
